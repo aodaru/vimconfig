0e26d4292906f955a607c42093eee9c4d291704d
Adal Garcia <aodarug@gmail.com>
2023-10-25 12:41:04 -0500

    retoques de configuracion

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,49 +1,52 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+  return
 end
 
 ts.setup({
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	ensure_installed = {
-		"markdown",
-		"markdown_inline",
-		"tsx",
-		"typescript",
-		"toml",
-		"php",
-		"twig",
-		"json",
-		"yaml",
-		"swift",
-		"css",
-		"html",
-		"lua",
-		"astro",
-		"javascript",
-	},
-	autotag = {
-		enable = true,
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
+  ensure_installed = {
+    "markdown",
+    "markdown_inline",
+    "tsx",
+    "typescript",
+    "toml",
+    "php",
+    "twig",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua",
+    "astro",
+    "javascript",
+  },
+  sync_install = false,
+  ignore_install = {},
+  auto_install = true,
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  autotag = {
+    enable = true,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 
--- Configuración para PHP
-parser_config.php = {
-	enabled = true,
-	parser = { "php", "twig" },
-	filetype = { "php", "twig" },
-}
+-- -- Configuración para PHP
+-- parser_config.php = {
+--   enabled = true,
+--   parser = { "php", "twig" },
+--   filetype = { "php", "twig" },
+-- }

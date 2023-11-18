cb37dfaae8fdf1497d37b9720a37a1267b96a9a4
Adal Garcia <aodarug@gmail.com>
2023-11-18 08:57:07 -0500

    Change Setup to LazyVim

--- init.lua
+++ init.lua
@@ -1,19 +1,2 @@
-require("aod.base")
-require("aod.highlights")
-require("aod.maps")
-require("aod.plugins")
-
-local has = vim.fn.has
-local is_mac = has("macunix")
-local is_win = has("win32")
-local is_wsl = has("wsl")
-
-if is_mac == 1 then
-	require("aod.macos")
-end
-if is_win == 1 then
-	require("aod.windows")
-end
-if is_wsl == 1 then
-	require("aod.wsl")
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")

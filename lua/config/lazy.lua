2a4116a6241dd0cd0e723fd879f08ce6d6081dae
aodaru <aodarug@gmail.com>
2024-07-15 12:44:51 -0500

    Configuracion de diccionario Espanol

---
+++ lua/config/lazy.lua
@@ -0,0 +1,111 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+	spec = {
+		-- add LazyVim and import its plugins
+		-- add LazyVim and import its plugins
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+			opts = {
+				colorscheme = "gruvbox",
+				news = {
+					lazyvim = true,
+					neovim = true,
+				},
+			},
+		},
+		-- import any extras modules here
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+		-- { import = "lazyvim.plugins.extras.lang.markdown" },
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
+		-- import/override with your plugins
+		{ import = "plugins" },
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	-- install = { colorscheme = { "tokyonight", "habamax" } },
+	-- dev = {
+	-- 	path = "~/.ghq/github.com",
+	-- },
+	checker = { enabled = true }, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
+
+require("gruvbox").setup({
+	transparent_mode = true,
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		emphasis = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	overrides = {
+		SignColumn = {
+			-- underline = true,
+			undercurl = true,
+			underdouble = true,
+			underdotted = true,
+			underdashed = true,
+			strikethrough = true,
+		},
+	},
+})
+vim.cmd("colorscheme gruvbox")
+
+-- require("conform").setup({
+-- 	format = {
+-- 		timeout_ms = 3000,
+-- 		async = false, -- not recommended to change
+-- 		quiet = false, -- not recommended to change
+-- 		lsp_format = "fallback", -- not recommended to change
+-- 	},
+-- 	formatters_by_ft = {
+-- 		lua = { "stylua" },
+-- 		javascript = { "prettier" },
+-- 		markdown = { "prettier" },
+-- 	},
+-- 	formatters = {
+-- 		injected = { options = { ignore_errors = true } },
+-- 	},
+-- })
+
+-- require("notify").setup({
+-- 	background_colour = "#000000",
+-- })
---
+++ lua/config/options.lua
@@ -0,0 +1,68 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.mapleader = " "
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = "C:\\Users\\agarcia\\scoop\\shims\\python3.EXE"
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = "pwsh"
+vim.opt.shellxquote = ""
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+vim.opt.shellquote = ""
+vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+-- vim.opt.termguicolors = true
+
+-- Undercurl linux
+vim.cmd([[let &t_Cs = "`e[4:3m"]])
+vim.cmd([[let &t_Ce = "`e[4:0m"]])
+
+if vim.fn.has("termguicolors") == 1 then
+	vim.opt.termguicolors = true
+	-- Enable undercurl
+	vim.api.nvim_set_var("t_Cs", "`e[4:3m")
+	vim.api.nvim_set_var("t_Ce", "`e[4:0m")
+end
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
+
+vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+
+if vim.fn.has("nvim-0.8") == 1 then
+	vim.opt.cmdheight = 0
+end

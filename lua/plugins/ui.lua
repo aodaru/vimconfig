1632d3cc397f6ad43badc5683f18e8a1379b802f
aodaru <aodarug@gmail.com>
2024-11-24 13:54:53 -0500

    Update Version

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -124,6 +124,12 @@ return {
 					groups = {
 						InclineNormal = { guibg = "#1d2021", guifg = "#f9f5d7" },
 						InclineNormalNC = { guifg = "#fbf1c7", guibg = "#282828" },
+						DashboardHeader = { guibg = "#1d2021", guifg = "#574893" },
+						DashboardDesc = { guibg = "#1d2021", guifg = "#282" },
+						DashboardKey = { guibg = "#1d2021", guifg = "#282" },
+						DashboardIcon = { guibg = "#1d2021", guifg = "#282" },
+						DashboardShortCut = { guibg = "#1d2021", guifg = "#282" },
+						DashboardFooter = { guibg = "#1d2021", guifg = "#282" },
 					},
 				},
 				window = { margin = { vertical = 0, horizontal = 1 } },
@@ -156,10 +162,30 @@ return {
 		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
 	},
 
+	-- {
+	-- 	"nvimdev/dashboard-nvim",
+	-- 	lazy = false,
+	-- 	event = "VimEnter",
+	-- 	opts = function(_, opts)
+	-- 		local logo = [[
+	--     ████████╗███████╗ █████╗ ██████╗  █████╗ ██████╗ ████████╗██╗   ██╗██████╗ ███████╗██╗   ██╗
+	--        ██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗   ██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝██║   ██║
+	--        ██║   █████╗  ███████║██████╔╝███████║██████╔╝   ██║    ╚████╔╝ ██║  ██║█████╗  ██║   ██║
+	--        ██║   ██╔══╝  ██╔══██║██╔═══╝ ██╔══██║██╔═██║    ██║      ██╔╝  ██║  ██║██╔══╝  ╚██╗ ██╔╝
+	--        ██║   ███████╗██║  ██║██║     ██║  ██║██║  ██╗   ██║      ██║   ██████╔╝███████╗ ╚████╔╝
+	--        ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═════╝ ╚══════╝  ╚═══╝
+	--      ]]
+	--
+	-- 		logo = string.rep("\n", 3) .. logo .. "\n\n"
+	-- 		opts.config.header = vim.split(logo, "\n")
+	-- 	end,
+	-- },
+
 	{
 		"nvimdev/dashboard-nvim",
+		lazy = false,
 		event = "VimEnter",
-		opts = function(_, opts)
+		opts = function()
 			local logo = [[
      ████████╗███████╗ █████╗ ██████╗  █████╗ ██████╗ ████████╗██╗   ██╗██████╗ ███████╗██╗   ██╗
         ██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗   ██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝██║   ██║
@@ -167,10 +193,113 @@ return {
         ██║   ██╔══╝  ██╔══██║██╔═══╝ ██╔══██║██╔═██║    ██║      ██╔╝  ██║  ██║██╔══╝  ╚██╗ ██╔╝
         ██║   ███████╗██║  ██║██║     ██║  ██║██║  ██╗   ██║      ██║   ██████╔╝███████╗ ╚████╔╝ 
         ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═════╝ ╚══════╝  ╚═══╝  
-      ]]
+    ]]
 
-			logo = string.rep("\n", 3) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
+			logo = string.rep("\n", 1) .. logo .. "\n\n"
+
+			local opts = {
+				theme = "doom",
+				hide = {
+					-- this is taken care of by lualine
+					-- enabling this messes up the actual laststatus setting after loading a file
+					statusline = false,
+				},
+				config = {
+					header = vim.split(logo, "\n"),
+        -- stylua: ignore
+					center = {
+						{
+							action = "lua LazyVim.pick()()",
+							desc = " Find File",
+							icon = " ",
+							key = "f",
+						},
+						{
+							action = "ene | startinsert",
+							desc = " New File",
+							icon = " ",
+							key = "n",
+						},
+						{
+							action = 'lua LazyVim.pick("oldfiles")()',
+							desc = " Recent Files",
+							icon = " ",
+							key = "r",
+						},
+						{
+							action = 'lua LazyVim.pick("live_grep")()',
+							desc = " Find Text",
+							icon = " ",
+							key = "g",
+						},
+						{
+							action = "lua LazyVim.pick.config_files()()",
+							desc = " Config",
+							icon = " ",
+							key = "c",
+						},
+						{
+							action = 'lua require("persistence").load()',
+							desc = " Restore Session",
+							icon = " ",
+							key = "s",
+						},
+						{
+							action = "LazyExtras",
+							desc = " Lazy Extras",
+							icon = " ",
+							key = "x",
+						},
+						{
+							action = "Lazy",
+							desc = " Lazy",
+							icon = "󰒲 ",
+							key = "l",
+						},
+						{
+							action = function()
+								vim.api.nvim_input("<cmd>qa<cr>")
+							end,
+							desc = " Quit",
+							icon = " ",
+							key = "q",
+						},
+					},
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"🎉 ⚡ Neovim loaded "
+								.. stats.loaded
+								.. "/"
+								.. stats.count
+								.. " plugins in "
+								.. ms
+								.. "ms",
+						}
+					end,
+				},
+			}
+
+			for _, button in ipairs(opts.config.center) do
+				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+				button.key_format = "  %s"
+			end
+
+			-- open dashboard after closing lazy
+			if vim.o.filetype == "lazy" then
+				vim.api.nvim_create_autocmd("WinClosed", {
+					pattern = tostring(vim.api.nvim_get_current_win()),
+					once = true,
+					callback = function()
+						vim.schedule(function()
+							vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+						end)
+					end,
+				})
+			end
+
+			return opts
 		end,
 	},
 }

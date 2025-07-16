ba08d02c97d5a4a52149d0e89395c961c247ebe8
aodaru <aodarug@gmail.com>
2025-07-16 09:09:46 -0500

    Retoques

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -82,21 +82,55 @@ return {
 		opts = {
 			options = {
 				mode = "tabs",
-				-- separator_style = "slant",
+				separator_style = "slant",
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 			},
 		},
 	},
 
+	-- battery usage
+	{
+		"justinhj/battery.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" },
+		config = function()
+			local nvimbattery = require("battery")
+			nvimbattery.setup({
+				update_rate_seconds = 30, -- Number of seconds between checking battery status
+				show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
+				show_plugged_icon = true, -- If true show a cable icon alongside the battery icon when plugged in
+				show_unplugged_icon = false, -- When true show a diconnected cable icon when not plugged in
+				show_percent = true, -- Whether or not to show the percent charge remaining in digits
+				vertical_icons = true, -- When true icons are vertical, otherwise shows horizontal battery icon
+				multiple_battery_selection = 1, -- Which battery to choose when multiple found. "max" or "maximum", "min" or "minimum" or a number to pick the nth battery found (currently linux acpi only)
+			})
+		end,
+	},
 	-- statusline
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		opts = {
 			options = {
-				-- globalstatus = false,
+				globalstatus = false,
 				theme = "gruvbox_dark",
+				disabled_filetypes = { statusline = { "dashboard", "snacks_dashboard" } },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_z = {
+					{
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+					{
+						function()
+							return require("battery").get_status_line()
+						end,
+						color = { fg = "#fe8019", bg = "#3c383" },
+					},
+				},
 			},
 		},
 	},
